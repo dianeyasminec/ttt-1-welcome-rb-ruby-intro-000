60f7eeca71ef88bf3e77cd0f78b06c8b27641305
Diane Compaore <dianeyasminec60@gmail.com>
2021-03-02 04:02:55 +0000

    Automatically backed up by Learn

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -1,5 +1,1 @@
-puts 1+2
-puts 5 * (12-8) + -15
-name = 'Patricia Rosanna Jessica Mildred Oppenheimer'
-puts 'My name is ' + name + '.'
-puts 'Wow!  ' + name + ' is a really long name!'
+puts "Welcome to Tic Tac Toe!"
